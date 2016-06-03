0d0eeccea4b1b64641b6f90d40c69cd9cf706f5d
david.fang <mw_fang@163.com>
2016-06-03 10:07:13 +0800

    docs/lib/page/MyDataPage.jsx

---
+++ docs/lib/page/MyDataPage.jsx
@@ -0,0 +1,42 @@
+'use strict';
+
+import React from "react";
+import ReStock from "react-stockcharts";
+
+import ContentSection from "lib/content-section";
+import Row from "lib/row";
+import Section from "lib/section";
+
+import CandleStickChartWithUpdatingData from "lib/charts/CandleStickChartWithUpdatingData";
+import KagiWithUpdatingData from "lib/charts/KagiWithUpdatingData";
+import PointAndFigureWithUpdatingData from "lib/charts/PointAndFigureWithUpdatingData";
+import RenkoWithUpdatingData from "lib/charts/RenkoWithUpdatingData";
+
+var { helper: { TypeChooser } } = ReStock;
+
+
+var MyDataPage = React.createClass({
+	statics: {
+		title: 'Updating Data'
+	},
+	render() {
+		return (
+			<ContentSection title={MyDataPage.title}>
+				<Row>
+					<Section colSpan={2} >
+						<aside dangerouslySetInnerHTML={{__html: require('md/UPDATING-DATA')}}></aside>
+					</Section>
+				</Row>
+				<Row  title={MyDataPage.title}>
+					<Section colSpan={2}>
+						<TypeChooser>
+							{(type) => <CandleStickChartWithUpdatingData data={this.props.someData} type={type} />}
+						</TypeChooser>
+					</Section>
+				</Row>
+			</ContentSection>
+		);
+	}
+});
+
+export default MyDataPage;
